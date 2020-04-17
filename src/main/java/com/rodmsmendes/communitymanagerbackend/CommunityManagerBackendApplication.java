17a81bd1455e0bafdd8c459df4e4f0f6b9e9aab5
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2020-04-17 13:26:26 -0300

    Initial commit

---
+++ src/main/java/com/rodmsmendes/communitymanagerbackend/CommunityManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rodmsmendes.communitymanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodmsmendes/communitymanagerbackend/CommunityManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodmsmendes.communitymanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
